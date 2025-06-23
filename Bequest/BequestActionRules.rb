e5e1dac650cc72a2a03824aa9941b5876f0192c2
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-06-23 17:08:16 -0400

    Update BequestActionRules.rb

--- Bequest/BequestActionRules.rb
+++ Bequest/BequestActionRules.rb
@@ -153,28 +153,30 @@ module BequestAction
   end
 
   class Player
-    print "> "; action = gets.chomp
+    def self.parser
+      print "> "; action = gets.chomp
 
-    if    action ==   "1"; Game::ActionRules.diety
-    elsif action ==   "2"; Game::ActionRules.physical
-    elsif action ==   "3"; Game::ActionRules.magical
-    elsif action ==   "4"; Game::ActionRules.location
+      if    action ==   "1"; Game::ActionRules.diety
+      elsif action ==   "2"; Game::ActionRules.physical
+      elsif action ==   "3"; Game::ActionRules.magical
+      elsif action ==   "4"; Game::ActionRules.location
 
-    # These allow you to chain actions together.
-    elsif action == "1-2"; Game::ActionRules.diety;      Game::ActionRules.physical
-    elsif action == "1-3"; Game::ActionRules.diety;      Game::ActionRules.magical
-    elsif action == "1-4"; Game::ActionRules.diety;      Game::ActionRules.location
-    elsif action == "2-1"; Game::ActionRules.physical;   Game::ActionRules.diety
-    elsif action == "2-3"; Game::ActionRules.physical;   Game::ActionRules.magical
-    elsif action == "2-4"; Game::ActionRules.physical;   Game::ActionRules.location
-    elsif action == "3-1"; Game::ActionRules.magical;    Game::ActionRules.diety
-    elsif action == "3-2"; Game::ActionRules.magical;    Game::ActionRules.physical
-    elsif action == "3-4"; Game::ActionRules.magical;    Game::ActionRules.location
-    elsif action == "4-1"; Game::ActionRules.location;   Game::ActionRules.diety
-    elsif action == "4-2"; Game::ActionRules.location;   Game::ActionRules.physical
-    elsif action == "4-3"; Game::ActionRules.location;   Game::ActionRules.magical
-    else
-      puts "Unrecgonized action..."
+      # These allow you to chain actions together.
+      elsif action == "1-2"; Game::ActionRules.diety;      Game::ActionRules.physical
+      elsif action == "1-3"; Game::ActionRules.diety;      Game::ActionRules.magical
+      elsif action == "1-4"; Game::ActionRules.diety;      Game::ActionRules.location
+      elsif action == "2-1"; Game::ActionRules.physical;   Game::ActionRules.diety
+      elsif action == "2-3"; Game::ActionRules.physical;   Game::ActionRules.magical
+      elsif action == "2-4"; Game::ActionRules.physical;   Game::ActionRules.location
+      elsif action == "3-1"; Game::ActionRules.magical;    Game::ActionRules.diety
+      elsif action == "3-2"; Game::ActionRules.magical;    Game::ActionRules.physical
+      elsif action == "3-4"; Game::ActionRules.magical;    Game::ActionRules.location
+      elsif action == "4-1"; Game::ActionRules.location;   Game::ActionRules.diety
+      elsif action == "4-2"; Game::ActionRules.location;   Game::ActionRules.physical
+      elsif action == "4-3"; Game::ActionRules.location;   Game::ActionRules.magical
+      else
+        puts "Unrecgonized action..."
+      end
     end
   end
 
