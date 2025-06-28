8022ee959a6f6c6b5f4ad311181a0e2d248e35e3
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-06-28 13:42:27 -0400

    Add files via upload

---
+++ dialogue.rb
@@ -0,0 +1,6 @@
+require_relative "bequest"
+
+def standard_francophonic;    BequestDialogue::EachWord.standard_francophonic; end
+def adjust_expected_dialogue; BequestDialogue::EachWord.standard_francophonic; end
+def learn_dialogue;                  BequestDialogue::EachWord.learn_dialogue; end
+def guess_dialogue;                  BequestDialogue::EachWord.guess_dialogue; end
