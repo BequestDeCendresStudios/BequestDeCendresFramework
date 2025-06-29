4814dfe7fa3a001ccf74423964ae0cfdeedb0107
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-06-28 22:14:34 -0400

    Update spaceshipescape.rb

--- spaceshipescape.rb
+++ spaceshipescape.rb
@@ -1,5 +1,11 @@
 require_relative "Experimental/StealthGauntlet.rb"
 
+################################################################################
+#                                Sample Game                                   #
+################################################################################
+# This mostly serves for historical purposes to show the relationship to       #
+# Yumemoire and how this grew and evolved out of a Minetest mod.               #
+################################################################################
 def init_game
   StealthGauntlet::Yumemoire.initialize
 end
